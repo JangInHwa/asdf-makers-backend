1e8fa8c892718391a40b4f55c7281182a6520d3a
uglyonlytoday <superjang3357@naver.com>
2021-12-14 20:16:34 +0900

    config.settings.ALLOWED_HOSTS 수정 'asdf-makers.herokuapp.com' 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ import django_heroku
 import dj_database_url
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'asdf-makers.herokuapp.com']
 django_heroku.settings(locals())
 db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
