14885d8537f1f7365a45cccb20bf4eafdae6d93b
uglyonlytoday <superjang3357@naver.com>
2021-12-14 21:02:15 +0900

    makers.views Group 클래스명을 GroupViews로 변경

--- makers/views.py
+++ makers/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .serializers import GroupSerializer
-from .models import Application
+from .models import Application, Group
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
 from rest_framework.views import APIView
 
 # Create your views here.
-class Group(APIView):
+class GroupView(APIView):
 	authentication_classes = [TokenAuthentication]
 	permission_classes = [IsAuthenticated]
 	def post(self, request:Request):
@@ -23,3 +23,11 @@ class Group(APIView):
 			application.save()
 			return Response(GroupSerializer(group).data)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GroupDetailView(APIView):
+	authentication_classes = [TokenAuthentication]
+	permission_classes = [IsAuthenticated]
+	def get(self, request:Request, pk:int):
+		group = get_object_or_404(Group, pk=pk)
+		serializer = GroupSerializer(group)
+		return Response(serializer.data)
