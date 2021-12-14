70ba61d0cff35de1d9cd54fe8f7f62b611506625
uglyonlytoday <superjang3357@naver.com>
2021-12-14 22:37:31 +0900

    UserDetailView get에 유저의 application 정보를 볼 수 있도록 수정

--- account/serializers.py
+++ account/serializers.py
@@ -1,3 +1,4 @@
+from makers.serializers import ApplicationSerializer
 from rest_framework import serializers
 from .models import User
 
@@ -5,6 +6,7 @@ from .models import User
 # from .models import User
 class UserSerializer(serializers.ModelSerializer):
 	# email = seria
+	applications = ApplicationSerializer(many=True, read_only=True)
 	class Meta:
 		model = User
 		exclude = ('is_superuser','is_active', 'user_permissions')
