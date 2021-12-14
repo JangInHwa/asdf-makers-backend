e165004c355963b9cac01a57a1ba4b763ea35761
uglyonlytoday <superjang3357@naver.com>
2021-12-14 20:01:40 +0900

    models.py REQURED_FIELDS 설정 추가

--- account/models.py
+++ account/models.py
@@ -6,52 +6,52 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 
 class UserManager(BaseUserManager):
-    use_in_migrations = True
+	use_in_migrations = True
 
-    def _create_user(self, username, email, password, **extra_fields):
-        if not email:
-            raise ValueError('The given email must be set')
-        if not username:
-            raise ValueError('The given username must be set')
-        user: User = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+	def _create_user(self, username, email, password, **extra_fields):
+		if not email:
+			raise ValueError('The given email must be set')
+		if not username:
+			raise ValueError('The given username must be set')
+		user: User = self.model(username=username, email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
 
-    def create(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
+	def create(self, username, email, password=None, **extra_fields):
+		extra_fields.setdefault('is_superuser', False)
+		return self._create_user(username, email, password, **extra_fields)
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
+	def create_superuser(self, username, email, password=None, **extra_fields):
+		extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+		if extra_fields.get('is_superuser') is not True:
+			raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(username, email, password, **extra_fields)
+		return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(verbose_name='email',
-                              max_length=255,
-                              unique=True)
-    username = models.CharField(max_length=20)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True, blank=True)
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
+	id = models.AutoField(primary_key=True)
+	email = models.EmailField(verbose_name='email',
+							  max_length=255,
+							  unique=True)
+	username = models.CharField(max_length=20)
+	is_active = models.BooleanField(default=True)
+	date_joined = models.DateTimeField(auto_now_add=True, blank=True)
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['email', 'password']
 
-    objects = UserManager()
+	objects = UserManager()
 
-    class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
-        ordering = ['email']
+	class Meta:
+		verbose_name = 'user'
+		verbose_name_plural = 'users'
+		ordering = ['email']
 
-    def __str__(self) -> str:
-        return f'{self.username} : {self.email}'
+	def __str__(self) -> str:
+		return f'{self.username} : {self.email}'
 
-    @property
-    def is_staff(self):
-        return self.is_superuser
+	@property
+	def is_staff(self):
+		return self.is_superuser
