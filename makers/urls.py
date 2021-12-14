77df598553326810096fb9aabf24df22abb3bef8
uglyonlytoday <superjang3357@naver.com>
2021-12-14 21:02:30 +0900

    GroupDetailView url 매치

--- makers/urls.py
+++ makers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('groups', views.Group.as_view())
+	path('groups', views.GroupView.as_view()),
+	path('groups/<int:pk>', views.GroupDetailView.as_view())
 ]
